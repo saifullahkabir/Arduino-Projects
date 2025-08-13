5ac9eeda02d3908c0b962d3026eb062028b6edda
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-08-14 04:24:29 +0600

    pwm for analog

---
+++ PwmWithAnalog.ino
@@ -0,0 +1,103 @@
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+
+  analogWrite(3, 0);
+  delay(500);
+  analogWrite(3, 10);
+  delay(500);
+  analogWrite(3, 20);
+  delay(500);
+  analogWrite(3, 40);
+  delay(500);
+  analogWrite(3, 60);
+  delay(500);
+  analogWrite(3, 80);
+  delay(500);
+  analogWrite(3, 100);
+  delay(500);
+  analogWrite(3, 120);
+  delay(500);
+  analogWrite(3, 140);
+  delay(500);
+  analogWrite(3, 160);
+  delay(500);
+  analogWrite(3, 180);
+  delay(500);
+  analogWrite(3, 200);
+  delay(500);
+  analogWrite(3, 220);
+  delay(500);
+  analogWrite(3, 240);
+  delay(500);
+  analogWrite(3, 255);
+  delay(3000);
+
+
+  analogWrite(6, 0);
+  delay(500);
+  analogWrite(6, 10);
+  delay(500);
+  analogWrite(6, 20);
+  delay(500);
+  analogWrite(6, 40);
+  delay(500);
+  analogWrite(6, 60);
+  delay(500);
+  analogWrite(6, 80);
+  delay(500);
+  analogWrite(6, 100);
+  delay(500);
+  analogWrite(6, 120);
+  delay(500);
+  analogWrite(6, 140);
+  delay(500);
+  analogWrite(6, 160);
+  delay(500);
+  analogWrite(6, 180);
+  delay(500);
+  analogWrite(6, 200);
+  delay(500);
+  analogWrite(6, 220);
+  delay(500);
+  analogWrite(6, 240);
+  delay(500);
+  analogWrite(6, 255);
+  delay(3000);
+
+
+  analogWrite(9, 0);
+  delay(500);
+  analogWrite(9, 10);
+  delay(500);
+  analogWrite(9, 20);
+  delay(500);
+  analogWrite(9, 40);
+  delay(500);
+  analogWrite(9, 60);
+  delay(500);
+  analogWrite(9, 80);
+  delay(500);
+  analogWrite(9, 100);
+  delay(500);
+  analogWrite(9, 120);
+  delay(500);
+  analogWrite(9, 140);
+  delay(500);
+  analogWrite(9, 160);
+  delay(500);
+  analogWrite(9, 180);
+  delay(500);
+  analogWrite(9, 200);
+  delay(500);
+  analogWrite(9, 220);
+  delay(500);
+  analogWrite(9, 240);
+  delay(500);
+  analogWrite(9, 255);
+  delay(3000);
+}
